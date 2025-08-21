aa31cd6159cba0a332e47366fe7ef9fdb614ed51
hyuni86 <anbh345@gmail.com>
2025-08-21 20:47:01 +0900

    Update schemas.py

--- app/domains/users/schemas.py
+++ app/domains/users/schemas.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
 
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+    class Config:
+        from_attributes = True
+
+from sqlalchemy.orm import Session
+from . import models
+
+def get_users(db: Session):
+    return db.query(models.User).all()
